221b0f2002e1a44f24d8bcb961996c858e3b845e
emihhorn <emilyhopehorn@gmail.com>
2021-01-10 16:48:59 -0600

    27

--- js/exercises.js
+++ js/exercises.js
@@ -312,3 +312,121 @@ assert(isNegativeEven(negativeOddNumber), false, "Exercise 20");
 assert(isNegativeEven(negativeEvenNumber), true, "Exercise 20");
 addToDone("Exercise 20 is correct.")
 
+// Exercise 21
+// Write a function definition named half that takes in a number and returns half the provided number.
+function half(x) {
+    return x / 2;
+}
+
+assert(half(4), 2, "Exercise 21");
+assert(half(5), 2.5, "Exercise 21");
+assert(half(positiveOddNumber), positiveOddNumber / 2, "Exercise 21");
+assert(half(positiveEvenNumber), positiveEvenNumber / 2, "Exercise 21");
+assert(half(negativeOddNumber), negativeOddNumber / 2, "Exercise 21");
+assert(half(negativeEvenNumber), negativeEvenNumber / 2, "Exercise 21");
+addToDone("Exercise 21 is correct.")
+
+
+
+// Exercise 22
+// Write a function definition named double that takes in a number and returns double the provided number.
+function double (x) {
+    return x * 2;
+}
+
+assert(double(4), 8, "Exercise 22");
+assert(double(5), 10, "Exercise 22");
+assert(double(positiveOddNumber), positiveOddNumber * 2, "Exercise 22");
+assert(double(positiveEvenNumber), positiveEvenNumber * 2, "Exercise 22");
+assert(double(negativeOddNumber), negativeOddNumber * 2, "Exercise 22");
+assert(double(negativeEvenNumber), negativeEvenNumber * 2, "Exercise 22");
+addToDone("Exercise 22 is correct.")
+
+
+
+// Exercise 23
+// Write a function definition named triple that takes in a number and returns triple the provided number.
+function triple(x) {
+    return x * 3;
+}
+
+assert(triple(4), 12, "Exercise 23");
+assert(triple(5), 15, "Exercise 23");
+assert(triple(positiveOddNumber), positiveOddNumber * 3, "Exercise 23");
+assert(triple(positiveEvenNumber), positiveEvenNumber * 3, "Exercise 23");
+assert(triple(negativeOddNumber), negativeOddNumber * 3, "Exercise 23");
+assert(triple(negativeEvenNumber), negativeEvenNumber * 3, "Exercise 23");
+addToDone("Exercise 23 is correct.")
+
+
+
+// Exercise 24
+// Write a function definition named reverseSign that takes in a number and returns the provided number but with the sign reversed.
+function reverseSign(x) {
+    return x *= -1;
+}
+
+assert(reverseSign(4), -4, "Exercise 24");
+assert(reverseSign(-5), 5, "Exercise 24");
+assert(reverseSign(positiveOddNumber), positiveOddNumber * -1, "Exercise 24");
+assert(reverseSign(positiveEvenNumber), positiveEvenNumber * -1, "Exercise 24");
+assert(reverseSign(negativeOddNumber), negativeOddNumber * -1, "Exercise 24");
+assert(reverseSign(negativeEvenNumber), negativeEvenNumber * -1, "Exercise 24");
+addToDone("Exercise 24 is correct.")
+
+
+
+// Exercise 25
+// Write a function definition named absoluteValue that takes in a number and returns the absolute value of the provided number
+function absoluteValue(x) {
+    return Math.abs(x);
+}
+
+assert(absoluteValue(4), 4, "Exercise 25");
+assert(absoluteValue(-5), 5, "Exercise 25");
+assert(absoluteValue(positiveOddNumber), positiveOddNumber, "Exercise 25");
+assert(absoluteValue(positiveEvenNumber), positiveEvenNumber, "Exercise 25");
+assert(absoluteValue(negativeOddNumber), negativeOddNumber * -1, "Exercise 25");
+assert(absoluteValue(negativeEvenNumber), negativeEvenNumber * -1, "Exercise 25");
+addToDone("Exercise 25 is correct.")
+
+
+
+// Exercise 26
+// Write a function definition named isMultipleOfThree that takes in a number and returns true or false if the number is evenly divisible by 3.
+function isMultipleOfThree(x) {
+    if(x % 3 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+assert(isMultipleOfThree(3), true, "Exercise 26");
+assert(isMultipleOfThree(15), true, "Exercise 26");
+assert(isMultipleOfThree(9), true, "Exercise 26");
+assert(isMultipleOfThree(4), false, "Exercise 26");
+assert(isMultipleOfThree(10), false, "Exercise 26");
+addToDone("Exercise 26 is correct.")
+
+
+
+
+// Exercise 27
+// Write a function definition named isMultipleOfFive that takes in a number and returns true or false if the number is evenly divisible by 5.
+function isMultipleOfFive(x) {
+    if(x % 5 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+assert(isMultipleOfFive(3), false, "Exercise 27");
+assert(isMultipleOfFive(15), true, "Exercise 27");
+assert(isMultipleOfFive(9), false, "Exercise 27");
+assert(isMultipleOfFive(4), false, "Exercise 27");
+assert(isMultipleOfFive(10), true, "Exercise 27");
+addToDone("Exercise 27 is correct.")
+
+
