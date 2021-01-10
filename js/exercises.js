0842ec55530f8532c23322224aaaf89d0ef3186d
emihhorn <emilyhopehorn@gmail.com>
2021-01-10 15:58:56 -0600

    10 exercises

--- js/exercises.js
+++ js/exercises.js
@@ -1,139 +1,133 @@
-"use strict";
+"use strict"; // leave this line here :)
 
-/**
- * TODO:
- * Create a function called 'sayHello' that takes a parameter 'name'.
- * When called, the function should return a message that says hello to the passed in name.
- *
- * Example
- * > sayHello("codeup") // returns "Hello, codeup!"
- */
-function sayHello(name) {
-    return 'Hello' + name + '!';
-}
-console.log(sayHello(name:"codeup"))
+// Keep this function definition in order to test expected vs. actual results
+const assert = function(actual, expected, message = "") {
 
-/**
- * TODO:
- * Call the function 'sayHello' and pass your name as a string literal argument.
- * Store the result of the function call in a variable named 'helloMessage'.
- *
- * console.log 'helloMessage' to check your work
- */
-var helloMessage = sayHello(name: 'Emily');{
-    console.log(helloMessage);
+    // This compares primitive values and collections. If they differ, throw an error.
+    if(JSON.stringify(actual) !== JSON.stringify(expected)) {
+        console.error('[assert] expected:    ' + expected)
+        console.error('[assert] instead got: ' + actual)
+        throw Error("Assert failed in " + message);
+    }
+};
+
+
+// Keep this function here in order to add correct questions to the counter
+function addToDone(message) {
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(message);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    node.classList.add("finished");
+    document.querySelector(".correct ul").appendChild(node);
+    document.querySelector("#count").innerHTML = document.querySelectorAll('.finished').length + " of 101";
 }
 
-/**
- * TODO:
- * Store your name as a string in a variable named 'myName', and pass that
- * variable to the 'sayHello' function. You should see the same output in the
- * console.
- */
-var myName = 'Emily';
-console.log(sayHello(myName));
 
-// Don't modify the following line, it generates a random number between 1 and 3
-// and stores it in a variable named random
-var random = Math.floor((Math.random() * 3) + 1);
+// Welcome to 101 Exercises in JS
+// If you get "ReferenceError: someVariableName is not defined", that means you need to create a variable
 
+// Exercise #0, Example Problem:
+// Example problem setup: Create a variable named doingJSRightNow and assign it the boolean true.
+// The line below creates the variable named doingJSRightNow and assigns the boolean value true
+// To complete Exercise #0, uncomment the following line of JS
+var doingJSRightNow = true
 
+// The lines below will test your answer. If you see an error, then it means that your answer is incorrect or incomplete.
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Not_defined to understand this error message.
+assert(doingJSRightNow, true, "Exercise 0");
 
+//  Exercise 1
+// On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
+var onMarsRightNow = false
 
-/**
- * TODO:
- * Create a function called 'isTwo' that takes a number as a parameter.
- * The function should return a boolean value based on whether or not the passed
- * number is the number 2.
+assert(onMarsRightNow, false, "Exercise 1");
+addToDone("Exercise 1 is correct.");
 
-
- function
-
- * Example
- * > isTwo(1) // returns false
- * > isTwo(2) // returns true
- * > isTwo(3) // returns false
- *
- * Call the function 'isTwo' passing the variable 'random' as a argument.
- *
- * console.log *outside of the function* to check your work (you should see a
- * different result everytime you refresh the page if you are using the random
- * number)
- */
+// Exercise 2
+// For more on arrays, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
+// Create a variable named fruits and assign it an array of strings containing the following fruits.
+// mango, banana, guava, kiwi, and strawberry.
+var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
 
 
+assert(fruits, ["mango", "banana", "guava", "kiwi", "strawberry"], "Exercise 2");
+addToDone("Exercise 2 is correct.");
+
+// Exercise 3
+// Create a variable named vegetables and assign it an array of strings containing the following vegetables.
+// eggplant, broccoli, carrot, cauliflower, and zucchini
+var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"]
 
 
-function  isTwo(num) {
-    return num === 2;
-}
+assert(vegetables, ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"], "Exercise 3");
+addToDone("Exercise 3 is correct.");
 
-console.log(random)
-console.log(isTwo(random))
 
-/**
- * TODO:
- * Create a function named 'calculateTip' to calculate a tip on a bill at a
- * restaurant. The function should accept a tip percentage and the total of the
- * bill, and return the amount to tip
- *
- * Examples:
- * > calculateTip(0.20, 20) // returns 4
- * > calculateTip(0.25, 25.50) // returns 6.375
- * > calculateTip(0.15, 33.42) // returns 5.013
- */
+// Exercise 4
+// Create a variable named numbers and assign it an array of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function calculateTip(tipPercentage, totalBill){
-    return tipPercentage * totalBill;
-}
-console.log(calculateTip(0.20, 20), 4 ); //returns 4
+
+assert(numbers, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "Exercise 4");
+addToDone("Exercise 4 is correct.");
+
+
+// Exercise 5
+// Add the string "tomato" to the end of the fruits array.
+// *Hint* Recommend finding and using a built-in JS operation to add to an array rather than recreating the array
+fruits.push ("tomato");
 
 
 
+assert(fruits, ["mango", "banana", "guava", "kiwi", "strawberry", "tomato"], "Exercise 5");
+addToDone("Exercise 5 is correct");
 
-/**
- * TODO:
- * Use prompt and alert in combination with your calculateTip function to
- * prompt the user for the bill total and a percentage they would like to tip,
- * then display the dollar amount they should tip
- */
+// Exercise 6
+// add the string "tomato" onto the end of the vegetables array.
+// Recommend using the built-in JS operation to add to an array.
 
+vegetables.push ("tomato");
 
-
-
-function applyDiscount (orginalPrice, discountPercent){
-    return orginalPrice - (orginalPrice * discountPercent);
-}
-console.log(applyDiscount(orginalPrice: 100, discountPercent: .2));
+assert(vegetables,["eggplant", "broccoli", "carrot", "cauliflower", "zucchini", "tomato"], "Exercise 6");
+addToDone("Exercise 6 is correct")
 
 
-/**
- * TODO:
- * Create a function named `applyDiscount`. This function should accept a price
- * (before a discount is applied), and a discount percentage (a number between 0
- * and 1). It should return the result of applying the discount to the original
- * price.
- *
- * Example:
- * > var originalPrice = 100;
- * > var dicountPercent = .2; // 20%
- * > applyDiscount(originalPrice, dicountPercent) // 80
- *
- * > applyDiscount(45.99, 0.12) // 40.4712
- */
+// Exercise 7
+// Given the array of numbers defined below, reverse the array of numbers that you created above.
 
-var totalBill =prompt(message:"How much is your totatl Bill");
-var tipPercentage = prompt(message:"How much is your tip percentage ?");
+var someNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function calculateTip (tipPercentage, totalBill){
-    var globalVar = ((totalBill * tipPercentage) / 100 );
-    alert("Your tip amount is, $" + globalVar);
-    return ((totalBill * tipPercentage) / 100 );
+someNumbers.reverse();
+assert(someNumbers, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], "Exercise 7")
+addToDone("Exercise 7 is correct")
 
-}
 
-console.log(calculateTip(totalBill, tipPercentage));
 
-</script>
-</body>
-</html>
+// Exercise 8
+// Sort the vegetables in alphabetical order. Recommend finding a way to sort the array with a built-in method
+vegetables.sort();
+
+assert(vegetables, ['broccoli', 'carrot', 'cauliflower', 'eggplant', 'tomato', 'zucchini'], "Exercise 8")
+addToDone("Exercise 8 is correct.")
+
+// Exercise 9
+// Write the code necessary to sort the fruits in reverse alphabetical order
+
+fruits.sort();
+fruits.reverse();
+
+
+assert(fruits, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana'], "Exercise 9")
+addToDone("Exercise 9 is correct.")
+
+
+// Exercise 10
+// Write the code necessary to produce a single array that holds all fruits then all vegetables in the order as they were sorted above.
+// Assign the result to a variable named fruitsAndVeggies.
+// *hint* the search engine search here would be "how to combine two arrays in JavaScript", for example.
+
+var fruitsAndVeggies = fruits.concat(vegetables);
+
+
+assert(fruitsAndVeggies, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana', 'broccoli', 'carrot', 'cauliflower', 'eggplant', 'tomato', 'zucchini'], "Exercise 10")
+addToDone("Exercise 10 is correct")
