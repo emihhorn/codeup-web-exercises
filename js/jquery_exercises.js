94654892f9eb09fc665577f6418654c242c6a2b7
emihhorn <emilyhopehorn@gmail.com>
2021-02-03 11:50:17 -0600

    jquery

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,6 +4,19 @@ $(document).ready(function (){
     alert("Can you guess the name of this Movie?")
 })
 
+
+// document.getElementById("movieStatement").style.background = "palevioletred";
+
+var texts = Array.from(document.getElementsByClassName("filler-text"));
+
+texts.forEach(function (text) {
+    text.style.color = "palevioletred";
+});
+
+// document.getElementById("dolor").innerHTML = ";)";
+
+
+
 // $('.codeup').css('border', '1px solid red');
 //
 // $('li').css('font-size', '20px');
@@ -15,7 +28,7 @@ $(document).ready(function (){
 // alert(document.getElementById("first-heading".innerHTML));
 // alert($("second-heading").html())
 
-$("li").css("font-size", "20px");
+// $("li").css("font-size", "20px");
 
 // $("h1").css("background-color", "blue");
 // $("p").css("background-color", "pink");
@@ -24,8 +37,34 @@ $("li").css("font-size", "20px");
 
 // alert($('h1').html());//or .text
 
-$("h1").each(function (){
-    alert($(this).html())
-});
+// $("h1").each(function (){
+//     alert($(this).html())
+// });
+//
+// $('h1, p, li').css({"background-color": "turquoise"});
 
-$('h1, p, li').css({"background-color": "turquoise"});
+
+
+
+
+
+$('h1').click(
+    function() {
+        $(this).css('background-color', '#F439A1');
+    }
+);
+
+$('p').dblclick(
+    function() {
+        $(this).css('font-size', '18px');
+    }
+);
+
+$('li').hover(
+    function() {
+        $('li').css('color', 'red');
+    },
+    function() {
+        $('li').css('color', 'teal');
+    }
+);
