c762b87d7deac8a3bbf091a437a99d59ec3290cb
emihhorn <emilyhopehorn@gmail.com>
2021-02-08 23:04:07 -0600

    flo town

--- js/geocode-utils.js
+++ js/geocode-utils.js
@@ -7,10 +7,11 @@ function geocode(search, token) {
         .then(function(res) {
             return res.json();
             // to get all the data from the request, comment out the following three lines...
-        // }).then(function(data) {
-        //     return data.features[0].center;
-        // });
-// }
+        })
+        .then(function(data) {
+            return data.features[0].center;
+        });
+}
 
 function reverseGeocode(coordinates, token) {
     var baseUrl = 'https://api.mapbox.com';
