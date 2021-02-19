4731ed16d42e3157462197b7f57e5fad8b200ad1
emihhorn <emilyhopehorn@gmail.com>
2021-02-18 21:38:10 -0600

    updates

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,24 +1,67 @@
-$.get("https://api.openweathermap.org/data/2.5/onecall", {
-    APPID: OWM_TOKEN,
-    lat: 34.1941,
-    lon: -79.7636,
-    units: "imperial",
-    exclude: "minutely, hourly"
-}).done(function(data) {})
+(function() {
+    "use strict"
+    var lat = 34.1941;
+    var lng = -79.7636;
+    var input = $("#user-input");
+//     lat: 34.1941,
+//     lon: -79.7636,
+    //ajax request
+    weatherMap();
+    function weatherMap() {
+        $.get("https://api.openweathermap.org/data/2.5/onecall", {
+            APPID: OWM_TOKEN,
+            lat: lat,
+            lon: lng,
+            units: "imperial",
+            exclude: "minutely,hourly"
+        }).done(function (data) {
+            console.log(data);
+            renderHtml(data);
+        });
+    }
+    //this function adds data to html bootstrap cards and displaya the weather
+    function renderHtml(data) {
+        var html = "";
+        for(var i = 0; i < 5; i += 1) {
+            var tempMax = data.daily[i].temp.max;
+            var tempMin = data.daily[i].temp.min;
+            var description = data.daily[i].weather[0].description;
+            var windSpeed = data.daily[i].wind_speed;
+            var pressure = data.daily[i].pressure;
+            var humidity = data.daily[i].humidity;
+            var iconCode = data.daily[i].weather[0].icon;
+            var date = data.daily[i].dt;
+            var date1 = new Date (date*1000);
+            var date2 = date1.toLocaleDateString("en-US");
+            html += "<div class='card' style='width: 17rem;'>";
+            html += "<div class='card-header text-center'>" + date2 + "</div>";
+            html += "<ul class='list-group list-group-flush'>";
+            html += "<li class='list-group-item text-center'>" + tempMax + "°F" + " / " + tempMin + "°F" + "<br>" + "<img src='http://openweathermap.org/img/wn/" + iconCode + ".png'>" ;
+            html += "<li class='list-group-item'>" + "Description: " + "<strong>" + description + "</strong>";
+            html += "<li class='list-group-item'>" + "Humidity: " + "<strong>" + humidity + "</strong>";
+            html += "<li class='list-group-item'>" + "Wind: " + "<strong>" + windSpeed + "</strong>";
+            html += "<li class='list-group-item'>" + "Pressure: " + "<strong>" + pressure + "</strong>";
+            html += "</ul>";
+            html += "</div>";
+        }
+        $("#weather").html(html);
+    }
+
+
+})();
+
+
+
+
+// $.get("https://api.openweathermap.org/data/2.5/onecall", {
+//     APPID: OWM_TOKEN,
+//     lat: 34.1941,
+//     lon: -79.7636,
+//     units: "imperial",
+//     exclude: "minutely, hourly"
+// }).done(function(data) {})
     // console.log(data);
 //-79.76, 34.19
-
-
-
-
-
-
-
-
-
-
-
-
 
     // fetch(api)
     //     .then(response => {
@@ -73,9 +116,4 @@ $.get("https://api.openweathermap.org/data/2.5/onecall", {
 // let tempValue = data['main']['temp']
 // let descValue = data['weather'][0]['description']
 //17410251e7b42b9f75fd4e1e02e2b9d5
-
-
-
-
-
 
