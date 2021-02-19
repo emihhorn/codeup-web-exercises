3abcba031521ef4761a2bc1ac5b9a7e16170436e
emihhorn <emilyhopehorn@gmail.com>
2021-02-18 23:58:37 -0600

    esix

--- js/es6.js
+++ js/es6.js
@@ -55,43 +55,45 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach((user) => emails.push(user.email));
-users.forEach((user) => names.push(user.name));
-
+users.forEach((email) => emails.push(user.email));
+users.forEach((name) => names.push(user.name));
+console.log(emails);
+console.log(names);
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
-    const name = user.name;
-    const email = user.email;
-    const languages = user.languages;
+    const { name, email, languages} = users;
+    // const name = user.name;
+    // const email = user.email;
+    // const languages = user.languages;
 
     // TODO: rewrite the assignment below to use template strings
-    developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+    developers.push(`${name}'s email is ${email} ${name} + knows ${languages.join(', ')}`);
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+for(let developer of developers) {
 
     // TODO: rewrite the assignment below to use template strings
-    list += '<li>' + developer + '</li>';
-});
+    list += `<li>'${developer}</li>`;
+};
 list += '</ul>';
 
-
+document.write(list);
 
 //TODO: Refactor the following code to use ES6 syntax. The output should stay the same.
 
-let message = '';
-const names = ['John', 'Paul', 'George', 'Ringo'];
+// let message = '';
+// const names = ['John', 'Paul', 'George', 'Ringo'];
 // for (var i = 0; i < names.length; i += 1) {
 //     message += 'Hello, ' + names[i] + '\n';
-for(let name of names) {
-    message += ` Hello, ${name}`;
-}
+// for(let name of names) {
+//     message += ` Hello, ${name}`;
+// }
