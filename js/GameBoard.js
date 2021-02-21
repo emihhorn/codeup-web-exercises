34bafa5e135d4922722a0eebd7213a2935c513b4
emihhorn <emilyhopehorn@gmail.com>
2021-02-20 23:44:01 -0600

    forEach loop

--- js/GameBoard.js
+++ js/GameBoard.js
@@ -6,4 +6,29 @@ class GameBoard {
         this.grid = [];
         this.DOMGrid = DOMGrid;
     }
+
+    showGameStatus(gameWin) {
+        const div = document.createElement('div')
+        div.classList.add('game-status')
+        div.innerHTML = `${gameWin ? 'WIN!' : 'GAME OVER!'}`
+        this.DOMGrid.appendChild(div);
+    }
+
+    createGrid(level) {
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid.innerHTML = '';
+        this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`;
+
+        level.forEach((square, i) =>{
+          const div.document.createElement('div');
+          div.classList.add('square', CLASS_LIST[square]);
+          div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px;`
+          this.DOMGrid.appendChild(div);
+          this.grid.push(div);
+
+
+          if (CLASS_LIST[square]=== OBJECT_TYPE.DOT)this.dotCount++;
+        })
+    }
 }
