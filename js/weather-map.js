a201a603a91e78206db0c29fc00f1154bf4b487e
emihhorn <emilyhopehorn@gmail.com>
2021-02-20 21:23:17 -0600

    edit

--- js/weather-map.js
+++ js/weather-map.js
@@ -24,17 +24,17 @@
     function renderHtml(data) {
         var html = "";
         for(var i = 0; i < 5; i += 1) {
-            var tempMax = data.daily[i].temp.max;
-            var tempMin = data.daily[i].temp.min;
-            var description = data.daily[i].weather[0].description;
-            var windSpeed = data.daily[i].wind_speed;
-            var pressure = data.daily[i].pressure;
-            var humidity = data.daily[i].humidity;
-            var iconCode = data.daily[i].weather[0].icon;
-            var date = data.daily[i].dt;
-            var date1 = new Date (date*1000);
-            var date2 = date1.toLocaleDateString("en-US");
-            html += "<div class='card' style='width: 17rem;'>";
+            let tempMax = data.daily[i].temp.max;
+            let tempMin = data.daily[i].temp.min;
+            let description = data.daily[i].weather[0].description;
+            let windSpeed = data.daily[i].wind_speed;
+            let pressure = data.daily[i].pressure;
+            let humidity = data.daily[i].humidity;
+            let iconCode = data.daily[i].weather[0].icon;
+            let date = data.daily[i].dt;
+            let date1 = new Date (date*1000);
+            let date2 = date1.toLocaleDateString("en-US");
+            html += "<div class='card' style='width: 15rem;'>";
             html += "<div class='card-header text-center'>" + date2 + "</div>";
             html += "<ul class='list-group list-group-flush'>";
             html += "<li class='list-group-item text-center'>" + tempMax + "°F" + " / " + tempMin + "°F" + "<br>" + "<img src='http://openweathermap.org/img/wn/" + iconCode + ".png'>" ;
