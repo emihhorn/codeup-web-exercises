129e7f11f2b8329fb9139da498b66b54c3556085
emihhorn <emilyhopehorn@gmail.com>
2021-02-20 21:54:37 -0600

    pac-man

--- js/konami.js
+++ js/konami.js
@@ -1,4 +1,88 @@
-var global = {
+export const GRID_SIZE = 20;
+export const CELL_SIZE = 20;
+export const DIRECTIONS = {
+    ArrowLeft: {
+        code: 37,
+        movement: -1,
+        rotation: 180
+    },
+    ArrowUp: {
+        code: 38,
+        movement: -GRID_SIZE,
+        rotation: 270
+    },
+    ArrowRight: {
+        code: 39,
+        movement: 1,
+        rotation: 0
+    },
+    ArrowDown: {
+        code: 40,
+        movement: GRID_SIZE,
+        rotation: 90
+    }
+};
+
+export const OBJECT_TYPE = {
+    BLANK: 'blank',
+    WALL: 'wall',
+    DOT: 'dot',
+    BLINKY: 'blinky',
+    PINKY: 'pinky',
+    INKY: 'inky',
+    CLYDE: 'clyde',
+    PILL: 'pill',
+    PACMAN: 'pacman',
+    GHOST: 'ghost',
+    SCARED: 'scared',
+    GHOSTLAIR: 'lair'
+};
+
+// Lookup array for classes
+export const CLASS_LIST = [
+    OBJECT_TYPE.BLANK,
+    OBJECT_TYPE.WALL,
+    OBJECT_TYPE.DOT,
+    OBJECT_TYPE.BLINKY,
+    OBJECT_TYPE.PINKY,
+    OBJECT_TYPE.INKY,
+    OBJECT_TYPE.CLYDE,
+    OBJECT_TYPE.PILL,
+    OBJECT_TYPE.PACMAN,
+    OBJECT_TYPE.GHOSTLAIR
+];
+
+// prettier-ignore
+export const LEVEL = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+    1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1,
+    1, 7, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 7, 1,
+    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+    1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1,
+    1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1,
+    1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1,
+    0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0,
+    0, 0, 0, 1, 2, 1, 2, 1, 9, 9, 9, 9, 1, 2, 1, 2, 1, 0, 0, 0,
+    1, 1, 1, 1, 2, 1, 2, 1, 9, 9, 9, 9, 1, 2, 1, 2, 1, 1, 1, 1,
+    1, 0, 0, 0, 2, 2, 2, 1, 9, 9, 9, 9, 1, 2, 2, 2, 0, 0, 0, 1,
+    1, 1, 1, 1, 2, 1, 2, 1, 9, 9, 9, 9, 1, 2, 1, 2, 1, 1, 1, 1,
+    0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0,
+    0, 0, 0, 1, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 1, 0, 0, 0,
+    1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1,
+    1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1,
+    1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1,
+    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+    1, 7, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 7, 1,
+    1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1,
+    1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+
+
+
+let global = {
 
     konami: function() {
         var konamikeys = [38,38,40,40,37,39,37,39,66,65],
@@ -12,7 +96,7 @@ var global = {
                 return;
             };
 
-            key = e.keyCode;
+            // key = e.keyCode;
 
             // Begin watching if first key in sequence was pressed.
             if(!started){
