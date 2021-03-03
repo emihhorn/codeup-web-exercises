04277bb742a85e75e89a73266ef212ce19fdcc08
emihhorn <emilyhopehorn@gmail.com>
2021-03-02 19:51:37 -0600

    CRUDjs

---
+++ CRUD/script.js
@@ -0,0 +1,36 @@
+function onFormSubmit(){
+ let forData = readFormData();
+    insertNewRecord(forData);
+}
+
+function readFormData(){
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["empCode"] = document.getElementById("empCode").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
+
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employee list").getElementsByTagName('tbody'[0]);
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.empCode;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a>Edit</a>
+                        <a>Delete</a>`;
+}
+
+
+
+
+
+
