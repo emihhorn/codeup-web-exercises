bb08f43a41543ebab569c87ba3c550b8c8f9400b
emihhorn <emilyhopehorn@gmail.com>
2021-03-18 21:12:23 -0500

    circle

---
+++ js/circle.js
@@ -0,0 +1,33 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            return; // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+        }
+    };
+
+
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+
+
+})();
